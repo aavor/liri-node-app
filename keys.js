846e657029b8ffce53f37aadc6782d6e350e278a
Alfred Avor <kofi.avor@gmail.com>
2017-04-13 15:53:35 -0400

    first commit

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'qzA1xrIsFoD2gSkOwBWRNYL3p',
+  consumer_secret: '  FCSOVI4zBQ90uOLfZVC68HDpYV1glRVWUhDmEceMEdXb852UTL',
+  access_token_key: ' 26157404-6miTNW3Qecuu9WKcD01mCw4fVaE7MA5Dyoa6phkWs',
+  access_token_secret: 'BByYdl34GoSz0l3rlhK7nECD3jTVSfS9XKCj4SN4O1wTx',
+}
---
+++ liri.js
@@ -0,0 +1,27 @@
+var spotify = require('spotify');
+ 
+spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+ 
+    // Do something with 'data' 
+});
+
+var twitter = require('twitter');
+var keys = require("./keys.js") 
+ 
+var client = new twitter({
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
+});
+ 
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
